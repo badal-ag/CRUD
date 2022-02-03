b381e5d386d40a59f549719b0fd6b2763ddf016f
Badal Agarwal <badal.agarwal1369@gmail.com>
2022-02-03 12:23:17 +0530

    Firebase Database Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState, useEffect} from 'react';
+import { db } from './firebase-config';
+import { collection } from "firebase/firestore";
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+        
+    }
+
+    getUsers();
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJaQLT0hMkezfwDCpFa3khKIKSd-Vq554",
+    authDomain: "crud-bad.firebaseapp.com",
+    projectId: "crud-bad",
+    storageBucket: "crud-bad.appspot.com",
+    messagingSenderId: "553347574281",
+    appId: "1:553347574281:web:11c1ee82a5276042dae6d8",
+    measurementId: "G-CC09P8B3YF"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
